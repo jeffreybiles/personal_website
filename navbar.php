91c95a194681ac1cea3c736f6314a2470c1f55d6
jeffreybiles <bilesjeffrey@gmail.com>
2012-05-16 11:15:32 -0500

    More levels for the candy factory.

--- navbar.php
+++ navbar.php
@@ -2,6 +2,6 @@
 <div id="navbar">
     <a href="/">Home</a>
     <a href="/games">Games</a>
-    <a href="http://blog.jeffreybiles.com">Blog</a>
+<!--    <a href="http://blog.jeffreybiles.com">Blog</a>-->
     <a href="/resume">Resume</a>
 </div>
